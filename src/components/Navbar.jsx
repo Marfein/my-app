5a5b36bdc267268e2768454c2f55dc1af6c19976
Marfein <mitskevich10@mail.ru>
2022-02-16 15:17:06 +0900

    fix bags

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (<nav className='nav'>
+        <div> <a href="#top">Profile</a> </div>
+        <div> <a href="#top">Message</a> </div>
+        <div> <a href="#top"a>News</a> </div>
+        <div> <a href="#top">Music</a> </div>
+        <div> <a href="#top"a>Settings</a> </div>
+    </nav>);
+}
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (<div className='content'>
+<img src='https://cdn.logo.com/hotlink-ok/logo-social.png' alt='logo'></img>
+        <div>ava + desription</div>
+        <div>
+            My posts
+            <div>New posts</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    </div>);
+}
+export default Profile;
