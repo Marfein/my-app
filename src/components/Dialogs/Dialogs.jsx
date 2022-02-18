7e44fe80b53d1d078ffcf9c79f611bf346c08c74
Marfein <mitskevich10@mail.ru>
2022-02-18 19:19:06 +0900

    21

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import s from'./Dialogs.module.css';
+import s from './Dialogs.module.css';
+
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}> Troll</div>
+                <div className={s.dialog}> Aquaman</div>
+                <div className={s.dialog}> Greg</div>
+                <div className={s.dialog}> Margarita</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>hi</div>
+                <div className={s.message}>cool</div>
+                <div className={s.message}>nice</div>
+                <div className={s.message}>lol</div>
+            </div>
         </div>
     )
 }
