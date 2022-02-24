4cec61a78999813465139e20e9a0664958d026a0
Marfein <mitskevich10@mail.ru>
2022-02-24 20:13:37 +0900

    29

---
+++ src/components/Redux/state.js
@@ -0,0 +1,24 @@
+let state =
+{
+    profilePage:{
+        posts : [
+            {id: 1, message: 'hi, fuck you', likecounts: 1, dislikecounts: 3},
+            {id: 2, message: 'no, fuck you', likecounts: 5, dislikecounts: 6}
+        ]
+    },
+    dialogsPage:{
+        dialogs : [
+            {id: 1, name: 'Troll'},
+            {id: 2, name: 'Aguaman'},
+            {id: 3, name: 'Greg'},
+            {id: 4, name: 'Margaret'}
+        ],
+        messages : [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'cool'},
+            {id: 3, message: 'nice'},
+            {id: 4, message: 'lol'}]
+    }
+
+};
+export default state;
--- src/index.js
+++ src/index.js
@@ -2,25 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import state from './components/Redux/state';
 import reportWebVitals from './reportWebVitals';
-let posts = [
-    {id: 1, message: 'hi, fuck you', likecounts: 1, dislikecounts: 3},
-    {id: 2, message: 'no, fuck you', likecounts: 5, dislikecounts: 6}
-]
-let dialogs = [
-    {id:1,name:'Troll'},
-    {id:2,name:'Aguaman'},
-    {id:3,name:'Greg'},
-    {id:4,name:'Margaret'}
-]
-let messages = [
-    {id:1,message:'Hi'},
-    {id:2,message:'cool'},
-    {id:3,message:'nice'},
-    {id:4,message:'lol'}];
+
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state}/>
   </React.StrictMode>, document.getElementById('root')
 );
 
